d180dbe525963ffab19aa2b600dd49806ecae5a0
bagzaru <bagzaru3690@gmail.com>
2023-10-15 21:33:04 +0900

    [Feat]: 몬스터가 이제 돌진함

--- Assets/ActionTester/Scripts/RushEnemyStateMachine.cs
+++ Assets/ActionTester/Scripts/RushEnemyStateMachine.cs
@@ -1,33 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RushEnemyStateMachine : MonoBehaviour
 {
     public float moveRadius = 16f;
     public float rushRadius = 5f;
-    public float rushDistance = 8f;
+
+    public float rushCastingTime = 0.4f;
+    public float stunTime = 1f;
+    public float rushDelay = 2f;
+
+    private bool _isRushDelaying = false;
 
     private Mover _mover;
+    private Dasher _dasher;
 
     enum State
     {
         IDLE,
         MOVE,
         RUSH,
-        STUN,
     }
 
     private State state = State.IDLE;
 
     private GameObject target = null;
 
-    //IDLE<->MOVE->RUSH->STUN->IDLE
+    //IDLE<->MOVE->RUSH->IDLE
 
     private void Start()
     {
         _mover = GetComponent<Mover>();
+        _dasher = GetComponent<Dasher>();
         SetState(State.IDLE);
     }
 
@@ -41,6 +48,8 @@ public class RushEnemyStateMachine : MonoBehaviour
             case State.MOVE:
                 UpdateMove();
                 break;
+            case State.RUSH:
+                break;
             default:
                 break;
         }
@@ -52,8 +61,11 @@ public class RushEnemyStateMachine : MonoBehaviour
         //애니메이터의 기존 변수 false
         //state 변경
         state = next;
-        Debug.Log($"몬스터 State 변경 : {next}");
         //애니메이터의 새로운 변수 true
+
+        //Rush는 코루틴으로 진행되므로 Coroutine 실행
+        if (next == State.RUSH)StartCoroutine(RushCoroutine());
+        
     }
 
     GameObject FindPlayerInRadius(float radius)
@@ -88,10 +100,11 @@ public class RushEnemyStateMachine : MonoBehaviour
         //플레이어 탐색
         //플레이어가 돌진 사거리 안에 들어오면 돌진으로 넘어간다.
         GameObject rushTo = FindPlayerInRadius(rushRadius);
-        if (rushTo != null)
+        if (rushTo != null && !_isRushDelaying)
         {
             //돌진 가능
             _mover.StopMove();
+            
             SetState(State.RUSH);
             return;
         }
@@ -109,6 +122,30 @@ public class RushEnemyStateMachine : MonoBehaviour
         Vector3 v = target.transform.position - transform.position;
         v.y = 0f;
         _mover.Move(v.normalized);
+    }
+
+    IEnumerator RushCoroutine()
+    {
+        _isRushDelaying = true;
+        //대쉬 준비 애니메이션(있을 경우)
+
+        yield return new WaitForSeconds(rushCastingTime);
+        //대쉬
+        Vector3 v = target.transform.position - transform.position;
+        v.y = 0f;
+        _dasher.Dash(v.normalized);
+        while (_dasher.IsDashing)
+        {
+            yield return null;
+        }
+        
+        //대쉬 종료, STUN 애니메이션(있을 경우)
+        
+        yield return new WaitForSeconds(stunTime);
+        //STUN까지 끝남, IDLE로 상태 전환
+        SetState(State.IDLE);
+        yield return new WaitForSeconds(rushDelay);
+        _isRushDelaying = false;
     }
 
 }
